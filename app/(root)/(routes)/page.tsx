13cdcdb309e15af2711e6fb753e999d0ff8d794d
Organique0 <grabnar.luka@gmail.com>
2023-09-03 02:32:40 +0200

    display companions

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,13 +1,70 @@
 import Categories from "@/components/Categories";
 import SearchInput from "@/components/SearchInput";
 import prismadb from "@/lib/prismadb";
+import Companions from "@/components/Companions";
+import { Companion } from "@prisma/client";
 
-export default async function HomePage() {
+interface HomePageProps {
+  searchParams: {
+    categoryId: string
+    name: string
+  }
+}
+
+export default async function HomePage({ searchParams }: HomePageProps) {
+  //omg this is the best database query ever.
+  //it allows you to search the database and find all results containing the name of the character within the current category
+  //if there is no name in searchParams then returns all results
+
+  //TODO: load messages count
+  //there should be messages linked to each character in the database
+  var data: Companion[];
+  if (searchParams.name) {
+    data = await prismadb.companion.aggregateRaw({
+      pipeline: [
+        {
+          $search: {
+            index: "companionSearch",
+            autocomplete: {
+              path: "name",
+              query: searchParams.name
+            }
+          },
+        },
+        {
+          $addFields: {
+            categoryId: searchParams.categoryId
+          }
+        },
+        {
+          $match: {
+            categoryId: searchParams.categoryId
+          }
+        },
+        {
+          $sort: {
+            createdAt: -1
+          }
+        },
+        {
+          $limit: 50
+        }
+      ],
+    }) as unknown as Companion[];  //this is super important to get the correct data type
+  } else {
+    data = await prismadb.companion.findMany({
+      where: {
+        categoryId: searchParams.categoryId
+      }
+    })
+  }
+
   const categories = await prismadb.category.findMany();
   return (
     <div className="h-full p-4 space-y-2">
       <SearchInput />
       <Categories data={categories} />
+      <Companions data={data} />
     </div>
   )
 }
---
+++ components/Companions.tsx
@@ -0,0 +1,60 @@
+import { Companion } from "@prisma/client";
+import Image from "next/image";
+import { Card, CardFooter, CardHeader } from "./ui/card";
+import Link from "next/link";
+import { MessagesSquare } from "lucide-react";
+import { Item } from "@radix-ui/react-dropdown-menu";
+
+interface CompanionsProps {
+    data: (Companion & {
+        categoryId: string
+    })[];
+}
+
+const Companions = ({ data }: CompanionsProps) => {
+    if (data.length === 0) {
+        return (
+            <div className="pt-10 flex flex-col justify-center space-y-3 items-center">
+                <h5 className="text-sm text-muted-foreground">There were no results found. So here is a nice icon.</h5>
+                <div className="relative h-60 w-60">
+                    <Image fill className="grayscale" alt="empty" src="empty.svg" />
+                </div>
+            </div>
+        )
+    }
+    //TODO: Add messages count
+    return (
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-2 pb-10">
+            {data.map((comp) => (
+                <Card key={comp.id} className="bg-primary/10 rounded-xl cursor-pointer hover:opacity-75 transition border-0">
+                    <Link href={`/chat/${comp.id}`}>
+                        <CardHeader className="flex items-center justify-center text-center text-muted-foreground">
+                            <div className="relative h-32 w-32">
+                                <Image fill className="rounded-xl object-cover" alt="companion" src={comp.src} />
+                            </div>
+                            <p className="font-bold">
+                                {comp.name}
+                            </p>
+                            <p className="text-xs">
+                                {comp.description}
+                            </p>
+                        </CardHeader>
+                        <CardFooter className="flex justify-between text-xs text-muted-foreground items-center">
+                            <p className="lowecase">
+                                @{comp.username}
+                            </p>
+                            <div className="flex items-center">
+                                <MessagesSquare className="h-3 w-3 mr-1" />
+                                <p>
+                                    msCount
+                                </p>
+                            </div>
+                        </CardFooter>
+                    </Link>
+                </Card>
+            ))}
+        </div>
+    );
+}
+
+export default Companions;
